6295ecfd3eb8a570ba25646c3651ee90b72c6b80
Michael Vetter <g.bluehut@gmail.com>
2013-12-17 20:43:18 +0100

    Add class for figures and use it.
    I decided to make class for the figures which for now just hold the
    Unicode for the white figures.

---
+++ jessy/figures.java
@@ -0,0 +1,10 @@
+package jessy;
+
+public final class figures {
+	public static final char PAWN = '\u2659';
+	public static final char KNIGHT  = '\u2658';
+	public static final char BISHOP  = '\u2657';
+	public static final char ROOK  = '\u2656';
+	public static final char QUEEN  = '\u2655';
+	public static final char KING  = '\u2654';
+}
