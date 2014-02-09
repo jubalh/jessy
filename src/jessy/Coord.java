e872aebcfd360400e14a90614fbb4b84cc5153fb
Michael Vetter <g.bluehut@gmail.com>
2014-02-09 15:22:16 +0100

    adding javadoc for Coord and making parameters final

--- src/jessy/Coord.java
+++ src/jessy/Coord.java
@@ -4,56 +4,113 @@ public final class Coord {
 	int x;
 	int y;
 
+	/**
+	 * Empty Constructor.
+	 */
 	public Coord() {
 	}
 
-	public Coord(int x, int y) {
+	/**
+	 * Constructor initializing the coordinates.
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 */
+	public Coord(final int x, final int y) {
 		setX(x);
 		setY(y);
 	}
 
-	public void setX(int x) {
+	/**
+	 * Sets X coordinate.
+	 * @param x coordinate
+	 */
+	public void setX(final int x) {
 		this.x = x;
 	}
 
-	public void setY(int y) {
+	/**
+	 * Sets y coordinate.
+	 * @param y coordinate
+	 */
+	public void setY(final int y) {
 		this.y = y;
 	}
 	
+	/**
+	 * Gets X coordinate.
+	 * @return x coordinate
+	 */
 	public int getX() {
 		return this.x;
 	}
 
+	/**
+	 * Gets Y coordinate.
+	 * @return y coordinate
+	 */
 	public int getY() {
 		return this.y;
 	}
 	
-	public int increaseX(int amount) {
+	/**
+	 * Increase x coordinate by amount.
+	 * @param amount
+	 * @return new x value
+	 */
+	public int increaseX(final int amount) {
 		return this.x += amount;
 	}
 
-	public int increaseY(int amount) {
+	/**
+	 * Increase y coordinate by amount.
+	 * @param amount
+	 * @return new y value
+	 */
+	public int increaseY(final int amount) {
 		return this.y += amount;
 	}
 	
-	public void increase(int amount) {
+	/**
+	 * Increases X and Y by amount.
+	 * @param amount
+	 */
+	public void increase(final int amount) {
 		increaseX(amount);
 		increaseY(amount);
 	}
 
-	public int decreaseX(int amount) {
+	/**
+	 * Decreases x coordinate by amount.
+	 * @param amount
+	 * @return new x value
+	 */
+	public int decreaseX(final int amount) {
 		return this.x -= amount;
 	}
 
-	public int decreaseY(int amount) {
+	/**
+	 * Decreases y coordinate by amount.
+	 * @param amount
+	 * @return new y value
+	 */
+	public int decreaseY(final int amount) {
 		return this.y -= amount;
 	}
 	
-	public void decrease(int amount) {
+	/**
+	 *  Decrase x and y coordinate by amount.
+	 * @param amount
+	 */
+	public void decrease(final int amount) {
 		decreaseX(amount);
 		decreaseY(amount);
 	}
 
+	/**
+	 * Checks whether two coordinates are the same.
+	 * @param coord to check with oneself
+	 * @return true if the same
+	 */
 	public boolean equals(final Coord coord) {
 		if (coord.x == this.x && coord.y == this.y) {
 			return true;
