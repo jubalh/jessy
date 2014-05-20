e8b703b6d0254cfcbccaacf1fe5497086e8def20
Michael Vetter <g.bluehut@gmail.com>
2014-05-20 20:52:20 +0200

    a little further the BoardTest

--- test/jessy/BoardTest.java
+++ test/jessy/BoardTest.java
@@ -13,13 +13,22 @@ public class BoardTest {
 
 		matrix = b.getMatrix();
 
+		// test checkboard field size
 		assertEquals(matrix.length, 8);
-
+		int col;
 		for(Figure[] figures : matrix) {
+			col = 0;
 			for(Figure figure : figures) {
 				assertEquals(figure, null);
+				col++;
 			}
+			assertEquals(col, 8);
 		}
+		
+		// check setting of figure
+		b.setFigure(new Coord(1,5), new King());
+		matrix = b.getMatrix();
+		assertEquals(matrix[0][4].getClass(), new King().getClass());
 	}
 
 }
