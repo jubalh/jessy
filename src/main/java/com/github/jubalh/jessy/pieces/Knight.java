7ea0dea2cc5c0b991764b508319ff90cf0e096ef
Natalie Prange <flashmops@gmail.com>
2014-06-10 15:47:09 +0200

    Knight.java: finished canMove(), involves issue #1

--- src/main/java/com/github/jubalh/jessy/pieces/Knight.java
+++ src/main/java/com/github/jubalh/jessy/pieces/Knight.java
@@ -3,6 +3,7 @@ package com.github.jubalh.jessy.pieces;
 import com.github.jubalh.jessy.Board;
 import com.github.jubalh.jessy.Color;
 import com.github.jubalh.jessy.Coord;
+import com.github.jubalh.jessy.NotAField;
 
 public final class Knight extends Figure {
 
@@ -18,7 +19,29 @@ public final class Knight extends Figure {
 		return '\u2658';
 	}
 
-	public boolean canMove(Board board, Coord coordOld, Coord coordNew) {
-		return true; //edit here for issue #1
+	public boolean canMove(final Board board, Coord current, Coord cnew) {
+		Coord coordCurrent = new Coord(current.getX(), current.getY());
+		Coord coordNew = new Coord(cnew.getX(), cnew.getY());
+
+	    try {
+            // up and down
+            if (Math.abs(coordNew.getY() - coordCurrent.getY()) == 1 && Math.abs(coordNew.getX() - coordCurrent.getX()) == 2) {
+            	if (!isAllowedMove(board, coordCurrent, coordNew)) {
+					return false;
+				}
+				return true;
+			}
+
+            // right and left
+            if (Math.abs(coordNew.getY() - coordCurrent.getY()) == 2 && Math.abs(coordNew.getX()- coordCurrent.getX()) == 1) {
+                if (!isAllowedMove(board, coordCurrent, coordNew)) {
+					return false;
+				}
+				return true;
+            }
+		} catch (NotAField ex) {
+			// TODO
+		}
+		return false;
 	}
 }
