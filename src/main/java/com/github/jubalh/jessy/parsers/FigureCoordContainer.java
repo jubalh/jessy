3526cabf88ec308624d4540c1a462a1cbda8811f
Michael Vetter <g.bluehut@gmail.com>
2014-10-22 17:07:05 +0200

    rename ParseHelper to FigureCoordContainer to make it clearer

--- src/main/java/com/github/jubalh/jessy/parsers/FigureCoordContainer.java
+++ src/main/java/com/github/jubalh/jessy/parsers/FigureCoordContainer.java
@@ -1,9 +1,16 @@
+/**
+ * 
+ * A Container to hold a Figure and Coord object which belong together.
+ * @author Michael Vetter
+ *
+ */
+
 package com.github.jubalh.jessy.parsers;
 
 import com.github.jubalh.jessy.Coord;
 import com.github.jubalh.jessy.pieces.Figure;
 
-public class ParseHelper {
+public class FigureCoordContainer {
 	public Figure figure;
 	public Coord coord;
 }
--- src/main/java/com/github/jubalh/jessy/parsers/JessyNotationParser.java
+++ src/main/java/com/github/jubalh/jessy/parsers/JessyNotationParser.java
@@ -19,18 +19,18 @@ public class JessyNotationParser extends NotationParser {
 	public Move parse(String text) throws NotAField {
 		int index=0;
 		Move m = null;
-		ParseHelper pa = new ParseHelper();
+		FigureCoordContainer fc = new FigureCoordContainer();
 
-		index = parseFigurePos(text, pa);
+		index = parseFigurePos(text, fc);
 
 		// "-"  = move figure
 		char c = text.charAt(index);
 		if (c == '-') {
 			String sub = text.substring(++index);
-			ParseHelper pa2 = new ParseHelper();
+			FigureCoordContainer fc2 = new FigureCoordContainer();
 			// get second figure + position
-			index = parseFigurePos(sub, pa2);
-			m = new Move(pa.coord, pa2.coord);
+			index = parseFigurePos(sub, fc2);
+			m = new Move(fc.coord, fc2.coord);
 		}
 		return m;
 	}
@@ -41,18 +41,18 @@ public class JessyNotationParser extends NotationParser {
 	 * @param pa ParseHelper object, to return figure and position
 	 * @return number of characters that got passed, length of the string if proper.
 	 */
-	private int parseFigurePos(final String text, final ParseHelper pa) throws NotAField {
+	private int parseFigurePos(final String text, final FigureCoordContainer fc) throws NotAField {
 		int index = 0;
 		char c = text.charAt(index);
 
 		// figure
 		if (isUpperCase(c)) {
-			pa.figure = Figure.getFigureByChar(c);
-			if (pa.figure == null)
+			fc.figure = Figure.getFigureByChar(c);
+			if (fc.figure == null)
 				return index; // TODO Exception?
 			index++;
 		} else {
-			pa.figure = new Pawn();
+			fc.figure = new Pawn();
 		}
 
 		c = text.charAt(index);
@@ -61,7 +61,7 @@ public class JessyNotationParser extends NotationParser {
 			throw new NotAField();
 
 		String sub = text.substring(index, index+2);//TODO: +2..
-		pa.coord = new Coord(sub);
+		fc.coord = new Coord(sub);
 		index += 2;
 
 		return index;
